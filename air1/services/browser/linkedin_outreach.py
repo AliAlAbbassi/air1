26553b03e2beb2831f273edc2a53a96b2295ef07
Cuthlehoop <alialabbassi2001@gmail.com>
2025-11-15 21:29:26 +0200

    init linkedin_outreach

---
+++ air1/services/browser/linkedin_outreach.py
@@ -0,0 +1,13 @@
+"""
+LinkedinOutreach: connects with people on linkedin.
+Automated outreach, but done sequentially to emulate human behavior and avoid bot detection and rate limits.
+
+Will include a faster option: fast_connect(). Don't care about it right now tho.
+"""
+
+
+class LinkedinOutreach:
+
+    @staticmethod
+    def connect(self, profile_username: str):
+        pass
